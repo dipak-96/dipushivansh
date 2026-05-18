2ef52535741441e31735d1700eafd21ba7614fc8
dipu ojha <dipuojha272@gmail.com>
2026-05-18 12:00:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("contactForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    const res = await fetch("/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+
+    // WhatsApp open
+    window.open(result.url, "_blank");
+});
